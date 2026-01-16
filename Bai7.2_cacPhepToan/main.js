6d9b04b1c25c8cd2658c9cae68c326d3d5b28a03
phamngocduy142005@gmail.com <phamngocduy142005@gmail.com>
2026-01-16 16:02:30 +0700

    các phép toán cơ bản và thứ tự ưu tiên

---
+++ Bai7.2_cacPhepToan/main.js
@@ -0,0 +1,22 @@
+// các phép toán cơ bản:
+let a =5;
+let b=2;
+let tong =a+b;
+let hieu =a-b;
+let tich =a*b;
+let thuong =a/b;
+let  soDu=a%b;
+// xuất kết quả.
+console.log("tổng hai số" +tong);
+console.log("hieu hai số" +hieu);
+console.log("tich hai số" +tich);
+console.log("thuong hai số" +thuong);
+console.log("so du hai số" +soDu);
+
+// thứ tự ưu tiên:
+/*
+    - 1. trong ().
+    - 2. phải sang trái.
+    - 3. trái sang phải.
+......
+*/
