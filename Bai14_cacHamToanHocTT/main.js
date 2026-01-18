1f5ff1556fbf842c0cd3d7dbc1a8e0b691efe6ed
phamngocduy142005@gmail.com <phamngocduy142005@gmail.com>
2026-01-18 15:31:22 +0700

    hàm toán tiếp theo 2

--- Bai14_cacHamToanHocTT/main.js
+++ Bai14_cacHamToanHocTT/main.js
@@ -10,7 +10,7 @@
     3) Hàm Number.isNaN : phương thức này chỉ trả về `true` nếu value h
 hoặc biểu thức truyền vào có giá trị "NaN".
         (*) nó khác hoàn toàn hàm isNaN.
-
+    4) Hàm `Math.random()` : hàm lấy số ngẫu nhiên từ 0 cho đến sát 1 [0 -> 1).
 */
 
 // 1) hàm number
@@ -40,9 +40,23 @@ console.log(isNaN("  ")); // false -> giá trị nó ra 0.
 
 // 3) hàm kiểm tra NaN.
 console.log(Number.isNaN(NaN)); // true -> vì nó trả về NaN.
-console.log(Number.isNaN("abc"/"abc"));
-console.log("abc"/"abc");
-console.log(0/0);
+console.log(Number.isNaN("abc" / "abc"));
+console.log("abc" / "abc");
+console.log(0 / 0);
 console.log(Number.Nan);
 console.log({}); // false
 console.log("ponyfoo"); // false
+
+// 4) hàm lấy số ngẫu nhiên.
+// trường hợp 1:
+let randomValue = Math.random();
+console.log(`lấy số ngẫu nhiên từ 0 đến sát 1 là : ${randomValue}`);
+// trường hợp 2: lấy số lớn hơn 1.
+let randomValues = Math.random() * 10;
+console.log(`lấy số ngẫu nhiên từ 0 đến sát 1 là : ${randomValues}`);
+// trường hơp 3: số nguyên.
+let randomValue2 = parseInt(Math.random() * 10);
+console.log(`lấy số ngẫu nhiên từ 0 đến sát 1 là : ${randomValue2}`);
+// trường hợp 4: 0 đến sát 30.
+let randomValue3 =parseInt( Math.random() * 30);
+console.log(`lấy số ngẫu nhiên từ 0 đến sát 1 là : ${randomValue3}`);
