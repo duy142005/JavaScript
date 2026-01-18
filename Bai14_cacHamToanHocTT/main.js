3b9db8e208883ad0fcc2773037dda54b7689ddcc
phamngocduy142005@gmail.com <phamngocduy142005@gmail.com>
2026-01-18 15:03:22 +0700

    hàm toán tiếp theo

---
+++ Bai14_cacHamToanHocTT/main.js
@@ -0,0 +1,48 @@
+// các hàm tiếp theo.
+
+/*
+    1) Hàm Number(): chuyển value sang số, nếu ko chuyển được trả về NaN.
+    2) Hàm isNaN(value): kiểm tra xem giá trị (hoặc biểu thức) truyền vào:
+        + step 1: cố gắng chuyển đổi về kiểu number.
+        + step 2: 
+            -> trả về true, nếu gia trị sau chuyển đổi không phải là kiểu số.
+            -> trả về false, nếu giá trị sau chuyển đổi là kiểu số.
+    3) Hàm Number.isNaN : phương thức này chỉ trả về `true` nếu value h
+hoặc biểu thức truyền vào có giá trị "NaN".
+        (*) nó khác hoàn toàn hàm isNaN.
+
+*/
+
+// 1) hàm number
+let str = "123a";
+console.log(typeof str);
+// chuyển sang kiểu số
+let num = Number(str);
+console.log(num);
+console.log(typeof num);
+
+// 2) kiểm tra giá trị.
+// step 1:
+let a = "123";
+console.log(isNaN(a));
+// step 2:
+console.log(isNaN("abc")); // true
+console.log(isNaN("123")); // false -> 123 về số được.
+
+//trường hợp true , false
+let b = Number(true);
+console.log(b);
+console.log(isNaN(true)); // false -> true(luôn là 1) : false ( luôn là 0).
+
+console.log(isNaN(null)); // false -> giá trị nó ra 0.
+console.log(isNaN("")); // false -> giá trị nó ra 0.
+console.log(isNaN("  ")); // false -> giá trị nó ra 0.
+
+// 3) hàm kiểm tra NaN.
+console.log(Number.isNaN(NaN)); // true -> vì nó trả về NaN.
+console.log(Number.isNaN("abc"/"abc"));
+console.log("abc"/"abc");
+console.log(0/0);
+console.log(Number.Nan);
+console.log({}); // false
+console.log("ponyfoo"); // false
